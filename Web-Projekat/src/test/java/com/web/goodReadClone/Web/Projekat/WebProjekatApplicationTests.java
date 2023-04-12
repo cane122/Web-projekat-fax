4ead7916094b53f9565164df13450ae93f439063
cane122 <93400248+cane122@users.noreply.github.com>
2023-04-12 23:51:07 +0200

    adding basic initialized project

---
+++ Web-Projekat/src/test/java/com/web/goodReadClone/Web/Projekat/WebProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.goodReadClone.Web.Projekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
