a0023f0088d2d8a4b65956707074f7b21c02bf54
alekekres <alekekres@gmail.com>
2023-04-29 14:50:21 +0200

    commit for the tests

--- WebProject/src/main/java/goodReadClone/WebProject/repository/BookRepository.java
+++ WebProject/src/main/java/goodReadClone/WebProject/repository/BookRepository.java
@@ -3,5 +3,6 @@ package goodReadClone.WebProject.repository;
 import goodReadClone.WebProject.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
