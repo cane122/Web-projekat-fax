7b03b23bd00f9c98deb0d4733d973ec12602ab32
alekekres <alekekres@gmail.com>
2023-05-23 04:54:42 +0200

    Ispravak pretrage
    komentarisan search

--- src/main/java/goodReadClone/WebProject/repository/BookRepository.java
+++ src/main/java/goodReadClone/WebProject/repository/BookRepository.java
@@ -3,11 +3,13 @@ package goodReadClone.WebProject.repository;
 import goodReadClone.WebProject.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findByTitle(String title);
+    List<Book> findByTitleContainingIgnoreCase(String title);
 
 
 }
--- src/main/java/goodReadClone/WebProject/service/BookService.java
+++ src/main/java/goodReadClone/WebProject/service/BookService.java
@@ -5,6 +5,7 @@ import goodReadClone.WebProject.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,16 @@ public class BookService {
 
     public Book save(Book book){
         return bookRepository.save(book);
+    }
+
+//    public List<Book> search(String param){
+//        List<Book> temp = new ArrayList<>();
+//        for(int i = 0; i < bookRepository.findAll().size(); i++){
+//
+//        }
+//    }
+
+    public BookRepository getRepo(){
+        return bookRepository;
     }
 }
