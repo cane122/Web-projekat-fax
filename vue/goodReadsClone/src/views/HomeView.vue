7edb1e96af39a5a69678fe75e4f3b0ccd730841a
cane122 <cankovicognjen@gmail.com>
2023-06-23 18:05:09 +0200

    added logout button

--- vue/goodReadsClone/src/views/HomeView.vue
+++ vue/goodReadsClone/src/views/HomeView.vue
@@ -3,26 +3,46 @@
     <h1>Welcome to the Goodreads Clone</h1>
 
     <div class="button-group">
+      <!-- Existing router links -->
       <router-link to="/books" class="btn">Show Books</router-link>
-      <router-link to="/login" class="btn">Login</router-link>
+
       <router-link to="/unsigned-users" class="btn">Search users</router-link>
       <router-link v-if="author" to="/add-book" class="btn">Add Book</router-link>
       <router-link to="/register" class="btn">Register</router-link>
       <router-link to="/search-book" class="btn">Search</router-link>
       <router-link to="/genres" class="btn">Genres</router-link>
       <router-link to="/shelves" class="btn">Shelves</router-link>
+      
     </div>
+    <br><br>
+      <!-- Logout button -->
+      <router-link to="/login" class="btn">Login</router-link>
+      <button class="btn" @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "HomeView",
   data() {
     return {
-      author: localStorage.getItem('user_type') === 'Author' // Replace this with your logic to determine if the user is an author or not
+      author: localStorage.getItem('user_type') === 'Author'
     };
   },
+  methods: {
+    logout() {
+      axios.get('http://localhost:9090/logout',{withCredentials:true}) // Make a GET request to the "/logout" endpoint
+        .then(() => {
+          // Handle successful logout
+          localStorage.clear();
+          this.$router.push('/login');
+        })
+        .catch(error => {
+          console.error('Logout error:', error);
+        });
+    }
+  }
 };
 </script>
-
